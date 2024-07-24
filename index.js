51283470303f3f11206bc4f8ff152582e430b647
Hubert-a <hubertamarfio@gmail.com>
2024-07-24 11:24:36 +0000

    month list

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const calendar = document.querySelector(".calendar"),
   prev = document.querySelector(".prev"),
   next = document.querySelector(".next"),
   todayBtn = document.querySelector(".today-btn"),
+  monthBtn = document.querySelector(".month-btn"),
   gotoBtn = document.querySelector(".goto-btn"),
   dateInput = document.querySelector(".date-input"),
   eventDay = document.querySelector(".event-day"),
@@ -16,26 +17,6 @@ const calendar = document.querySelector(".calendar"),
   addEventFrom = document.querySelector(".event-time-from "),
   addEventTo = document.querySelector(".event-time-to "),
   addEventSubmit = document.querySelector(".add-event-btn ");
-
-let today = new Date();
-let activeDay;
-let month = today.getMonth();
-let year = today.getFullYear();
-
-const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
 
 // const eventsArr = [
 //   {
@@ -58,7 +39,25 @@ const months = [
 const eventsArr = [];
 getEvents();
 console.log(eventsArr);
+let today = new Date();
+let activeDay;
+let month = today.getMonth();
+let year = today.getFullYear();
 
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 //function to add days in days with class day and prev-date next-date on previous month and next month days and active on today
 function initCalendar() {
   const firstDay = new Date(year, month, 1);
@@ -125,6 +124,7 @@ function prevMonth() {
     month = 11;
     year--;
   }
+  updateEvents(new Date());
   initCalendar();
 }
 
@@ -134,6 +134,7 @@ function nextMonth() {
     month = 0;
     year++;
   }
+  updateEvents(new Date());
   initCalendar();
 }
 
@@ -198,6 +199,11 @@ todayBtn.addEventListener("click", () => {
   initCalendar();
 });
 
+monthBtn.addEventListener("click", () => {
+  //   alert("clicked");
+  getMonthsEvents();
+});
+
 dateInput.addEventListener("input", (e) => {
   dateInput.value = dateInput.value.replace(/[^0-9/]/g, "");
   if (dateInput.value.length === 2) {
@@ -236,6 +242,10 @@ function getActiveDay(date) {
   eventDay.innerHTML = dayName;
   eventDate.innerHTML = date + " " + months[month] + " " + year;
 }
+function getActiveMonth() {
+  eventDay.innerHTML = "Months Event";
+  eventDate.innerHTML = months[month] + " " + year;
+}
 
 //function update events when a day is active
 function updateEvents(date) {
@@ -261,8 +271,39 @@ function updateEvents(date) {
   });
   if (events === "") {
     events = `<div class="no-event">
-            <h3>No Events</h3>
+            <h3>No Celebrations available</h3>
         </div>`;
+  }
+  eventsContainer.innerHTML = events;
+  saveEvents();
+}
+
+function getMonthsEvents() {
+  let events = "";
+  getActiveMonth();
+  eventsArr.forEach((event) => {
+    if (
+      //   date === event.day &&
+      month + 1 === event.month &&
+      year === event.year
+    ) {
+      event.events.forEach((event) => {
+        events += `<div class="event">
+              <div class="title">
+                <i class="fas fa-circle"></i>
+                <h3 class="event-title">${event.title}</h3>
+              </div>
+              <div class="event-time">
+                <span class="event-time">${event.time}</span>
+              </div>
+          </div>`;
+      });
+    }
+  });
+  if (events === "") {
+    events = `<div class="no-event">
+              <h3>No Celebrations available</h3>
+          </div>`;
   }
   eventsContainer.innerHTML = events;
   saveEvents();
@@ -288,26 +329,6 @@ addEventTitle.addEventListener("input", (e) => {
   addEventTitle.value = addEventTitle.value.slice(0, 60);
 });
 
-function defineProperty() {
-  var osccred = document.createElement("div");
-  osccred.innerHTML =
-    "A Project By <a href='https://www.youtube.com/channel/UCiUtBDVaSmMGKxg1HYeK-BQ' target=_blank>Open Source Coding</a>";
-  osccred.style.position = "absolute";
-  osccred.style.bottom = "0";
-  osccred.style.right = "0";
-  osccred.style.fontSize = "10px";
-  osccred.style.color = "#ccc";
-  osccred.style.fontFamily = "sans-serif";
-  osccred.style.padding = "5px";
-  osccred.style.background = "#fff";
-  osccred.style.borderTopLeftRadius = "5px";
-  osccred.style.borderBottomRightRadius = "5px";
-  osccred.style.boxShadow = "0 0 5px #ccc";
-  document.body.appendChild(osccred);
-}
-
-defineProperty();
-
 //allow only time in eventtime from and to
 addEventFrom.addEventListener("input", (e) => {
   addEventFrom.value = addEventFrom.value.replace(/[^0-9:]/g, "");
@@ -329,7 +350,7 @@ addEventTo.addEventListener("input", (e) => {
   }
 });
 
-//function to add event to eventsArr
+// function to add event to eventsArr
 addEventSubmit.addEventListener("click", () => {
   const eventTitle = addEventTitle.value;
   const eventTimeFrom = addEventFrom.value;
@@ -424,6 +445,15 @@ eventsContainer.addEventListener("click", (e) => {
     if (confirm("Are you sure you want to delete this event?")) {
       const eventTitle = e.target.children[0].children[1].innerHTML;
       eventsArr.forEach((event) => {
+        // if (
+        //   event.day !== activeDay
+        //   //   event.month !== month + 1 &&
+        //   //   event.year !== year
+        // ) {
+        //   alert("please select a day to delete");
+        //   //   getMonthsEvents();
+        //   return;
+        // }
         if (
           event.day === activeDay &&
           event.month === month + 1 &&
